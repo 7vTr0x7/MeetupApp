60ce71d81d8a01098a465a47dfa76cd497cc3334
7vTr0x7 <7vTrox7@gmail.com>
2024-07-16 16:13:23 +0530

    MeetupApp - frontend - Home

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 const Home = () => {
-  return <div></div>;
+  return (
+    <>
+      <div className="container py-3 ">
+        <div className="float-end py-2">
+          <select className="form-select">
+            <option value="">Select Event Type</option>
+            <option value="Offline">Offline</option>
+            <option value="Online">Online</option>
+            <option value="Both">Both</option>
+          </select>
+        </div>
+        <h1 className="fw-bold">Meetup Events</h1>
+
+        <div></div>
+      </div>
+    </>
+  );
 };
 
 export default Home;
