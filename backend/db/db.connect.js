7dc2839b728fb7961f35f200ebfae3cc74efa111
7vTr0x7 <7vTrox7@gmail.com>
2024-07-16 12:03:41 +0530

    MeetupApp - backend - db

---
+++ backend/db/db.connect.js
@@ -0,0 +1,17 @@
+require("dotenv").config({ path: "D:backend/.env" });
+const mongoose = require("mongoose");
+
+const mongoURI = process.env.MONGODB_URL;
+
+const initializeDatabase = async () => {
+  try {
+    const connection = await mongoose.connect(mongoURI);
+    if (connection) {
+      console.log("Connected to MongoDB");
+    }
+  } catch (error) {
+    console.log("Failed to connect to MongoDB", error);
+  }
+};
+
+module.exports = { initializeDatabase };
