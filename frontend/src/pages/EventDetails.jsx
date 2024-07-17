46703c9f280bcb0452fcde4f8d4ee36d99dfab83
7vTr0x7 <7vTrox7@gmail.com>
2024-07-17 11:48:45 +0530

    MeetupApp - frontend - EventDetails

--- frontend/src/pages/EventDetails.jsx
+++ frontend/src/pages/EventDetails.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import logo from "../images/logo.png";
 
 const EventDetails = () => {
-  return <div></div>;
+  return (
+    <main className="bg-body-tertiary">
+      <div className="container py-3 ">
+        <img
+          src={logo}
+          alt="logo"
+          style={{ height: "70px", margin: "0", padding: "0" }}
+        />
+        <div className="float-end pt-4">
+          <form>
+            <div className="input-group ">
+              <input
+                placeholder="⌕ Search by title and t..."
+                className="form-control"
+                type="text"
+              />
+            </div>
+          </form>
+        </div>
+        <hr />
+      </div>
+    </main>
+  );
 };
 
 export default EventDetails;
